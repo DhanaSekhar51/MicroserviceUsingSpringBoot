678cbf9d8c870140bdbf887c50faf911d4eff775
DhanaSekhar51 <66307813+DhanaSekhar51@users.noreply.github.com>
2020-08-02 19:51:15 +0530

    Add files via upload

---
+++ Zuul-API-gateway/src/test/java/com/microservice/zuul/ZuulAPIgateway/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.zuul.ZuulAPIgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
